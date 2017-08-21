9fb95dc7cdd774fc3509882845016f46f593fb20
Takashi Masuda <masutaka.net@gmail.com>
2017-08-21 22:09:08 +0900

    Update Package-Requires

--- helm-qiita.el
+++ helm-qiita.el
@@ -5,7 +5,7 @@
 ;; Author: Takashi Masuda <masutaka.net@gmail.com>
 ;; URL: https://github.com/masutaka/emacs-helm-qiita
 ;; Version: 1.0.1
-;; Package-Requires: ((helm "1.9.5"))
+;; Package-Requires: ((helm "2.8.2"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
